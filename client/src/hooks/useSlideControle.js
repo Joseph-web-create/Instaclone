f99630981a218d96b5fede845373a07a5ef09489
Aace <aacejovi@gmail.com>
2025-04-17 18:42:16 +0100

    make some correction

--- client/src/hooks/useSlideControle.js
+++ client/src/hooks/useSlideControle.js
@@ -4,7 +4,7 @@ export default function useSlideControle(ArrayPhotos) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const handlePrevious = useCallback(() => {
     setCurrentImageIndex(
-      (prev) => prev - 1 + (ArrayPhotos?.length % ArrayPhotos?.length)
+      (prev) => (prev - 1 + ArrayPhotos.length) % ArrayPhotos.length
     );
   }, [ArrayPhotos?.length]);
 
